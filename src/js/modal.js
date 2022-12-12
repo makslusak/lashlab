47952028620841912e154af3c3c3ef564fc5ace1
makslusak <106471649+makslusak@users.noreply.github.com>
2022-12-12 19:53:49 +0200

    Add styles & logic for modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,21 +1,28 @@
 const btnsRef = document.querySelectorAll('[data-modal-open]');
-const modalRef = document.querySelector('.modal');
+const backdropRef = document.querySelector('.backdrop');
 const btnCloseModalRef = document.querySelector('[data-modal-close]');
 
 btnsRef.forEach(btn => btn.addEventListener('click', openModal));
 
 function openModal(evt) {
+  backdropRef.addEventListener('click', backdropClick);
   btnCloseModalRef.addEventListener('click', closeModal);
-  modalRef.classList.add('js-menu-open');
-  modalRef.classList.remove('js-menu-close');
+  window.addEventListener('keydown', backdropClick);
+  backdropRef.classList.add('js-menu-open');
+  backdropRef.classList.remove('js-menu-close');
   window.scrollTo(top);
   noScrollBody();
 }
 function closeModal(evt) {
-  modalRef.classList.remove('js-menu-open');
-  modalRef.classList.add('js-menu-close');
+  backdropRef.classList.remove('js-menu-open');
+  backdropRef.classList.add('js-menu-close');
   scrollBody();
   btnCloseModalRef.removeEventListener('click', closeModal);
+  backdropRef.removeEventListener('click', backdropClick);
+  window.removeEventListener('keydown', backdropClick);
+}
+function backdropClick(evt) {
+  if (evt.target === evt.currentTarget || evt.code === 'Escape') closeModal();
 }
 
 export function noScrollBody() {
